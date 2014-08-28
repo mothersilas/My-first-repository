7f64ac8653a6e74f94717823ba50c9054c1bdd74
Kewani Welay <mothersilas>
2014-08-28 09:05:33 +0000

    This all is PAN Africanism!

--- HW.py
+++ HW.py
@@ -3,6 +3,12 @@ print("hello Ethiopia")
 
 print("hello Ethiopia")
 print("I was a cow boy for 15 years.")
+print("African Institute for Mathematical Sciences")
+print("Africa will be better developed!")
+print("We Africans!")
+print("We promote better Africa")
+
+
 
 
 
