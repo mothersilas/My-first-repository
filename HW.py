64bcca76563046ddad16f65a8ac11adf17183904
Kewani Welay <mothersilas>
2014-08-31 16:11:56 +0000

    ploting!

--- HW.py
+++ HW.py
@@ -1,16 +1,30 @@
- from numbers import Number
+from pylab import *
+from numpy import *
+from square_geom import *
+
+v_square_area = np.vectorize(square_area)
+v_square_perimeter = np.vectorize(square_perimeter)
+
+S = np.linspace(0,10) # our side lengths
+
+A = v_square_area(S)  # the areas
+P = v_square_perimeter(S)  # the perimeters
+
+plot(S, A, '-r', label="Area")
+plot(S, P, ':b', label="Perimeter")
+
+xlabel('side length')
+ylabel('geo values')
+title('Square Geo Properties')
+legend(loc='upper right')
+
+show()
 
 
- def area_rectangle(l, w : Number) -> Number:
-     """
-     Calculate the area of a rectangle given its dimensions.
-     @param l: the base length of the rectangle.
-     @param w: the height of the rectangle.
-     @return: the area of the rectangle.
 
-     """
-     area = l*w
-     return area
+
+
+
 
 
 
